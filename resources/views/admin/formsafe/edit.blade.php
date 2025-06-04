072247d563681cd01584f9ed20acf850fca7aab8
duocdd <dangduoc.itcnew@gmail.com>
2025-06-04 21:27:46 +0700

    show button on home

---
+++ resources/views/admin/formsafe/edit.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-8 p-r-0 title-margin-right">
+            <div class="page-header">
+                <div class="page-title">
+                    <h1>Chỉnh sửa FormSafe</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section id="main-content">
+        <div class="row">
+            <div class="col-lg-12">
+                @include('admin.layouts.flash-message')
+                <div class="card">
+                    <div class="card-body offset-1 col-lg-10">
+                        <div class="basic-form">
+                            <form role="form" method="POST" action="{{ route('admin.formsafe.update', $form->id) }}">
+                                @csrf
+                                @method('PUT')
+                                <div class="form-group">
+                                    <label>Tên</label>
+                                    <input type="text" name="name" value="{{ old('name', $form->name) }}" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label>Điện thoại</label>
+                                    <input type="text" name="phone" value="{{ old('phone', $form->phone) }}" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="email" name="email" value="{{ old('email', $form->email) }}" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label>Địa chỉ</label>
+                                    <input type="text" name="address" value="{{ old('address', $form->address) }}" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label>Nội dung</label>
+                                    <textarea name="content" class="form-control">{{ old('content', $form->content) }}</textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label>Mã sản phẩm</label>
+                                    <input type="text" name="product_code" value="{{ old('product_code', $form->product_code) }}" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label>Ngày</label>
+                                    <input type="date" name="by_date" value="{{ old('by_date', $form->by_date) }}" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label>Ảnh báo cáo</label>
+                                    <input type="text" name="report_images" value="{{ old('report_images', $form->report_images) }}" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label>Ảnh môi trường</label>
+                                    <input type="text" name="env_images" value="{{ old('env_images', $form->env_images) }}" class="form-control">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Cập nhật</button>
+                                <a href="{{ route('admin.formsafe.index') }}" class="btn btn-default">Quay lại</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -127,6 +127,7 @@
 {{--                <p class="subheading text-secondary">Browse our Top Trending: the hottest picks loved by all. </p>--}}
             </div>
             @if(!empty($hotProducts))
+            <div class="flat-sw-navigation wow fadeInUp" data-wow-delay="0.1s">
                 <div dir="ltr" class="swiper tf-sw-latest" data-preview="5" data-tablet="4" data-mobile="1"
                      data-space-lg="15" data-space-md="15" data-space="15" data-pagination="1" data-pagination-md="1"
                      data-pagination-lg="3" >
@@ -155,6 +156,8 @@
                     </div>
                     <div class="sw-pagination-latest sw-dots type-circle justify-content-center"></div>
                 </div>
+                <div class="nav-prev-collection d-none d-lg-flex nav-sw style-line nav-sw-left"><i class="icon icon-arrLeft"></i></div>
+                <div class="nav-next-collection d-none d-lg-flex nav-sw style-line nav-sw-right"><i class="icon icon-arrRight"></i></div>
             @endif
         </div>
     </section>
